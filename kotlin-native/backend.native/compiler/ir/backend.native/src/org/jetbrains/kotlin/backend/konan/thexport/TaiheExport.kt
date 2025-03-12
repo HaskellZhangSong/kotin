a7abd6e9068c883cf85d32bb69493cd6858c3c27
songzh <haskell.songzh@hotmail.com>
2025-03-12 15:52:03 +0800

    reformat code in TaiheExport.kt

--- kotlin-native/backend.native/compiler/ir/backend.native/src/org/jetbrains/kotlin/backend/konan/thexport/TaiheExport.kt
+++ kotlin-native/backend.native/compiler/ir/backend.native/src/org/jetbrains/kotlin/backend/konan/thexport/TaiheExport.kt
@@ -129,7 +129,7 @@ internal class TaiheApiExporter(
         }
     }
 
-    fun kotlinFunctionToTaiheFunction(e: ExportedElement, isGlobal: Boolean, belongClass : String? = null, typeKind: ClassKind? = null): FunDecl {
+    fun kotlinFunctionToTaiheFunction(e: ExportedElement, isGlobal: Boolean, belongClass: String? = null, typeKind: ClassKind? = null): FunDecl {
         val containsArkTsString: Boolean = e.declaration.annotations.iterator().asSequence().toList().map { it.toString() }.contains("@ArkTsString")
         val arkTsStringAnnotations = if (containsArkTsString) {
             listOf(Annotation("ArkTsString"))
@@ -187,15 +187,16 @@ internal class TaiheApiExporter(
                             val interfaceName = it.name
                             val functions = it.scope.elements.filter {
                                 var annoList = it.declaration.annotations.iterator().asSequence().toList().map { it.toString() }
-                                it.isFunction && annoList.contains("@ArkTsExportFunctionTaihe") }.filter {
-                                    var funDec = it.declaration as DeserializedSimpleFunctionDescriptor
-                                    funDec.getOverriddenDescriptors().size == 0
-                                }
-                            val constructors : List<ExportedElement> = it.scope.elements.filter {
+                                it.isFunction && annoList.contains("@ArkTsExportFunctionTaihe")
+                            }.filter {
+                                var funDec = it.declaration as DeserializedSimpleFunctionDescriptor
+                                funDec.getOverriddenDescriptors().size == 0
+                            }
+                            val constructors: List<ExportedElement> = it.scope.elements.filter {
                                 it.name == "<init>"
                             }
                             val taiheConstructors = constructors.map { func -> kotlinFunctionToTaiheFunction(func, true, it.name, cd.getKind()) }
-                            val taiheFunc = functions.map { kotlinFunctionToTaiheFunction(it, false ) }
+                            val taiheFunc = functions.map { kotlinFunctionToTaiheFunction(it, false) }
                             val classImpl = it.irSymbol.owner as IrClassImpl
                             val superTypesList = classImpl.superTypes.map {
                                 val irSimpleType = it as IrSimpleType
@@ -211,10 +212,10 @@ internal class TaiheApiExporter(
                             }.filter { !(it.scope.isEmpty() && it.typeName == "Any") }
                             val iface = InterfaceDecl(anno, interfaceName, superTypesList, taiheFunc)
                             output("${iface.toString(0)}")
-                            output(taiheConstructors.map{ it.toString(0) }.joinToString("\n"))
+                            output(taiheConstructors.map { it.toString(0) }.joinToString("\n"))
 
                             if (cd.getKind().isSingleton) {
-                                val instanceFunName : String = it.scope.elements[0].cname + "_instance"
+                                val instanceFunName: String = it.scope.elements[0].cname + "_instance"
                                 val annos = listOf(Annotation("inner_name", listOf("\"${instanceFunName}\"")), Annotation("singleton"))
                                 val taiheInstanceFun = FunDecl(annos, true, "instance_" + interfaceName, listOf(), Pair(null, RefType(listOf(), interfaceName)))
                                 output(taiheInstanceFun.toString(0))
